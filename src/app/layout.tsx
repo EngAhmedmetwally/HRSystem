a0282049a1df93760e78332881ce0b11a53dac6f
ahmed magdy <dev.metwally@gmail.com>
2025-11-25 16:41:23 +0000

    File changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
+import { FirebaseClientProvider } from '@/firebase';
 
 const APP_NAME = "HighClass HR Automation";
 const APP_DESCRIPTION = "A smart, cloud-based attendance and payroll system.";
@@ -41,8 +42,10 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className={cn("font-body antialiased min-h-screen")}>
-        {children}
-        <Toaster />
+        <FirebaseClientProvider>
+          {children}
+          <Toaster />
+        </FirebaseClientProvider>
       </body>
     </html>
   );
